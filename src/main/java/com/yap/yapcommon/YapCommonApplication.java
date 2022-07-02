8bb3b656672945771d04db844660455c63d8a32c
zhangxianbiao <zhangxianbiao91@163.com>
2022-07-02 12:03:09 +0800

    create project

---
+++ src/main/java/com/yap/yapcommon/YapCommonApplication.java
@@ -0,0 +1,13 @@
+package com.yap.yapcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YapCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yap/yapcommon/YapCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yap.yapcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
